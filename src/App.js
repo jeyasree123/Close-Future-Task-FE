7d40b78f20b0b0467e945271bff710aaae5943d8
Kiruthika <kiruthikaganesan757@gmail.com>
2026-04-15 11:11:12 +0530

    feat: Initialize project structure with authentication and team management features

---
+++ src/App.js
@@ -0,0 +1,41 @@
+import { BrowserRouter, Routes, Route } from "react-router-dom";
+import Login from "./pages/Login";
+import Signup from "./pages/Signup";
+import Dashboard from "./pages/Dashboard";
+import Teams from "./pages/Teams";
+import TeamDetails from "./pages/TeamDetails";
+import ProtectedRoute from "./routes/ProtectedRoute";
+
+function App() {
+  return (
+    <BrowserRouter>
+      <Routes>
+        <Route path="/login" element={<Login />} />
+        <Route path="/signup" element={<Signup />} />
+
+        <Route
+          path="/"
+          element={
+            <ProtectedRoute>
+              <Dashboard />
+            </ProtectedRoute>
+          }
+        />
+
+        <Route
+          path="/teams"
+          element={
+            <ProtectedRoute>
+              <Teams />
+            </ProtectedRoute>
+          }
+        />
+
+       <Route path="/teams/:teamId" element={<TeamDetails />} />
+      </Routes>
+      
+    </BrowserRouter>
+  );
+}
+
+export default App;
